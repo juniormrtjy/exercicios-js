90cca596006e32372c9dea440a5e7ba1eef76df5
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-09-11 18:30:30 -0300

    update

--- 02 - numbers/main.js
+++ 02 - numbers/main.js
@@ -1,20 +1,18 @@
-// const num = parseFloat(prompt('Escolha um número:'))
-const num = 55
+const num = Number(prompt('Escolha um número:'))
+// const num = 55
 document.body.innerHTML += `<p>Raíz quadrada: <span>${num ** 0.5}</span></p>`
 document.body.innerHTML += `<p>${num} é inteiro? <span>${Number.isInteger(
   num
 )}</span></p>`
-document.body.innerHTML += `<p> <span></span></p>`
+document.body.innerHTML += `<p>${num} é NaN? <span>${Number.isNaN(
+  num
+)}</span></p>`
 document.body.innerHTML += `<p>Arredondando para baixo: <span>${Math.floor(
   num
 )}</span></p>`
-document.body.innerHTML += `<p>Arredondando para cima <span>${Math.ceil(
+document.body.innerHTML += `<p>Arredondando para cima: <span>${Math.ceil(
   num
 )}</span></p>`
-document.body.innerHTML += `<p>Com duas casas decimais <span>${Number(
+document.body.innerHTML += `<p>Com duas casas decimais: <span>${Number(
   num
 ).toFixed(2)}</span></p>`
-document.body.innerHTML += `<p> <span></span></p>`
-
-let nadda = document.getElementById('nada')
-console.log(nadda.innerHTML)
