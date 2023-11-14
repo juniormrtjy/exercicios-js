144a286914509a7459d63e68e1f1e6d6351b1bae
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-11-14 16:12:30 -0300

    refatorando eu acho

--- 08 - to-do/assets/js/task.js
+++ 08 - to-do/assets/js/task.js
@@ -11,22 +11,23 @@ let isProgressActive = false
 // ADD TASK FUNCTION
 export function addTask() {
   const task = inputTask.value
-  if (!task) return alert('Você precisa digitar algo.')
+  if (!task && !isProgressActive) {
+    showUserMessage('Você precisa digitar algo.')
+    return
+  }
 
-  if (!isEdit) {
-    if (!isProgressActive) {
-      addTaskToDOM(task)
-      taskList.push({ task: task, checked: false })
-      alertMessage('Você adicionou uma tarefa.')
-    }
+  if (!isEdit && !isProgressActive) {
+    addTaskToDOM(task)
+    taskList.push({ task, checked: false })
+    showUserMessage('Você adicionou uma tarefa.')
+  } else if (isProgressActive) {
+    return
   } else {
-    if (!isProgressActive) {
-      editTaskInDOM(task)
-      taskList[editIndex].task = task
-      isEdit = false
-      editIndex = undefined
-      alertMessage('Você editou esta tarefa.')
-    }
+    editTaskInDOM(task)
+    taskList[editIndex].task = task
+    isEdit = false
+    editIndex = undefined
+    showUserMessage('Você editou esta tarefa.')
   }
 
   inputTask.focus()
@@ -40,20 +41,20 @@ export default function removeItem(event) {
 
   const taskIndex = getTaskIndex(el)
 
-  if (el.classList.contains('remove')) {
-    if (!isProgressActive) {
-      if (isEdit)
-        return alert('Você não pode deletar enquanto edita uma tarefa.')
-      el.parentNode.remove()
-      alertMessage('Você removeu esta tarefa.')
+  if (el.classList.contains('remove') && !isProgressActive) {
+    if (isEdit) {
+      showUserMessage('Você não pode deletar enquanto edita uma tarefa.')
+      return
     }
+    el.parentNode.remove()
+    showUserMessage('Você removeu esta tarefa.')
 
     taskList.splice(taskIndex, 1)
     setLocalStorage()
   }
 
   // EDIT TASK FUNCTION
-  if (el.classList.contains('edit_task')) {
+  if (el.classList.contains('edit_task') && !isProgressActive) {
     const taskItem = el.parentNode
     inputTask.value = taskItem.firstChild.textContent
     editIndex = taskIndex
@@ -62,15 +63,9 @@ export default function removeItem(event) {
   }
 
   // CHECK TASK
-  if (el.classList.contains('check')) {
+  if (el.classList.contains('check') && !isProgressActive) {
     editIndex = taskIndex
-    const isChecked = taskList[editIndex].checked
-    if (!isChecked) {
-      el.classList.toggle('checked')
-      checkTaskInDOM()
-      return
-    }
-
+    taskList[editIndex].checked = !taskList[editIndex].checked
     el.classList.toggle('checked')
     checkTaskInDOM()
   }
@@ -83,21 +78,19 @@ export function loadTasks() {
   // PRINT TASK IN DOM
   if (tasks) {
     tasks.forEach(task => {
-      addTaskToDOM(task.task)
+      addTaskToDOM(task.task, task.checked)
       taskList.push(task)
     })
-  }
-
-  for (let i in taskList) {
-    const check = document.querySelectorAll('.check')
-    if (tasks[i].checked) check[i].classList.add('checked')
   }
 
   createDeleteMessage()
 }
 
-function addTaskToDOM(task) {
+function addTaskToDOM(task, checked) {
   const paragraph = createTags(task)
+  if (checked) {
+    paragraph.querySelector('.check').classList.add('checked')
+  }
   section.appendChild(paragraph)
 }
 
@@ -117,31 +110,16 @@ function setLocalStorage() {
 }
 
 function checkTaskInDOM() {
-  let checkBtn = taskList[editIndex].checked
-  !checkBtn
-    ? (taskList[editIndex].checked = true)
-    : (taskList[editIndex].checked = false)
-
   setLocalStorage()
 }
 
-/* 
-
-Se na taskList estiver true, o icon referente precisa ser marcado. Como eu faço isso?
-eu preciso retornar todos os buttons disponíveis no load, ok. Eles vão poder ser acessados através do index deles e então, eu tenho controle sobre o que marcar.
-
-se taskList[0] == true ? check[0].classList.add('checked')
-
-
-*/
-
-function alertMessage(txt) {
+function showUserMessage(message, time = 30) {
   isProgressActive = true
   const progress = document.querySelector('#file')
   const messageBox = document.querySelector('.delete-message')
   const paragraph = document.querySelector('.delete-message p')
 
-  paragraph.textContent = txt
+  paragraph.textContent = message
   const progressBar = setInterval(() => {
     if (progress.value > 0) {
       messageBox.style.top = '10px'
@@ -150,7 +128,7 @@ function alertMessage(txt) {
       messageBox.style.top = '-500px'
       isProgressActive = false
     }
-  }, 15)
+  }, time)
 
   if (progress.value == 0) {
     progress.value = 100
